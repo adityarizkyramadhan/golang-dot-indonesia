4ddb67c026ae10bff4a5425d929c0c80979469d3
Aditya Rizky Ramadhan <adityarizky1020@gmail.com>
2024-10-05 00:20:44 +0700

    fix: add errror handling in redis

--- internal/infrastructure/cache/redis.go
+++ internal/infrastructure/cache/redis.go
@@ -8,15 +8,21 @@ import (
 
 var redisClient *redis.Client
 
-func NewRedis() *redis.Client {
+func NewRedis() (*redis.Client, error) {
 	if redisClient != nil {
-		return redisClient
+		return redisClient, nil
 	}
 	client := redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("REDIS_ADDR"),
 		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
 	})
+
+	_, err := client.Ping().Result()
+	if err != nil {
+		return nil, err
+	}
+
 	redisClient = client
-	return redisClient
+	return redisClient, nil
 }
