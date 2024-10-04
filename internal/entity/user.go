d7d29bcc7ab3b2678c0d6295d8cb8f8b1d662756
Aditya Rizky Ramadhan <adityarizky1020@gmail.com>
2024-10-05 00:42:34 +0700

    feat: add timestamps to user table

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,11 +1,13 @@
 package entity
 
 type User struct {
-	ID       *int    `gorm:"primaryKey;autoIncrement" json:"id"`
-	Username *string `gorm:"unique;not null" json:"username"`
-	Password *string `gorm:"not null" json:"-"`
-	Name     *string `gorm:"not null;size:255" json:"name"`
-	Role     *string `gorm:"not null;size:10;default:'user'" json:"role"`
+	ID        *int    `gorm:"primaryKey;autoIncrement" json:"id"`
+	Username  *string `gorm:"unique;not null" json:"username"`
+	Password  *string `gorm:"not null" json:"-"`
+	Name      *string `gorm:"not null;size:255" json:"name"`
+	Role      *string `gorm:"not null;size:10;default:'user'" json:"role"`
+	CreatedAt *string `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt *string `gorm:"autoUpdateTime" json:"updated_at"`
 }
 
 func (u *User) TableName() string {
